7d5c546cbe02d8ff9a11039b63d7aadb72a9149b
evgeny <gkanafing@gmail.com>
2025-09-13 16:12:54 +0600

    add hand comments

--- Game/Hand.h
+++ Game/Hand.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <tuple>
 
 #include "../Models/Move.h"
@@ -12,6 +12,7 @@ class Hand
     Hand(Board *board) : board(board)
     {
     }
+	// получение координат клетки, на которую кликнул игрок
     tuple<Response, POS_T, POS_T> get_cell() const
     {
         SDL_Event windowEvent;
@@ -30,16 +31,20 @@ class Hand
                 case SDL_MOUSEBUTTONDOWN:
                     x = windowEvent.motion.x;
                     y = windowEvent.motion.y;
+                    // определение индексов клетки / кнопки
                     xc = int(y / (board->H / 10) - 1);
                     yc = int(x / (board->W / 10) - 1);
+					// нажата кнопка отката хода
                     if (xc == -1 && yc == -1 && board->history_mtx.size() > 1)
                     {
                         resp = Response::BACK;
                     }
+					// нажата кнопка перезапуска игры
                     else if (xc == -1 && yc == 8)
                     {
                         resp = Response::REPLAY;
                     }
+					// выбрана клетка на доске
                     else if (xc >= 0 && xc < 8 && yc >= 0 && yc < 8)
                     {
                         resp = Response::CELL;
@@ -51,6 +56,7 @@ class Hand
                     }
                     break;
                 case SDL_WINDOWEVENT:
+					// изменение размера окна
                     if (windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
                     {
                         board->reset_window_size();
@@ -64,6 +70,7 @@ class Hand
         return {resp, xc, yc};
     }
 
+    // ожидание действия игрока
     Response wait() const
     {
         SDL_Event windowEvent;
@@ -74,23 +81,28 @@ class Hand
             {
                 switch (windowEvent.type)
                 {
+					// закрытие окна
                 case SDL_QUIT:
                     resp = Response::QUIT;
                     break;
+					// изменение размера окна
                 case SDL_WINDOWEVENT_SIZE_CHANGED:
                     board->reset_window_size();
                     break;
+					// нажатие кнопки мыши
                 case SDL_MOUSEBUTTONDOWN: {
                     int x = windowEvent.motion.x;
                     int y = windowEvent.motion.y;
                     int xc = int(y / (board->H / 10) - 1);
                     int yc = int(x / (board->W / 10) - 1);
+					// нажата кнопка перезапуска игры
                     if (xc == -1 && yc == 8)
                         resp = Response::REPLAY;
                 }
                 break;
                 }
                 if (resp != Response::OK)
+                    // нажата служебная кнопка
                     break;
             }
         }
